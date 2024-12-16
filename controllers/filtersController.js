28391d4b29e286aa17ef87fb49e1db283da1d971
djgoodys <47307908+djgoodys@users.noreply.github.com>
2024-12-15 23:35:11 -0800

    changed monggose.ObjectId to mongoose.Types.ObjectId

--- controllers/filtersController.js
+++ controllers/filtersController.js
@@ -95,7 +95,7 @@ try {
       //console.log("typeof="+typeof(fid))
       //console.log('Converted ID:', fid);
         //const filter = await Filters.findById(fid)
-        const filter = await Filters.find({ id: mongoose.ObjectId(ID) })
+        const filter = await Filters.find({ id: mongoose.Types.ObjectId(ID) })
         .then(filter => {
         if (!filter) {
           return res.status(404).json({ message: ID + ' Filter not found' });
